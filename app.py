452304358933ed01d6b444dc57a36241e9de2775
Tony Terrasa <tony.terrasa@gmail.com>
2024-03-27 17:53:18 -0400

    trim old code

--- app.py
+++ app.py
@@ -34,9 +34,6 @@ class WordForm4(FlaskForm):
         return True
 
 
-
-
-
 @app.route("/")
 def index():
     return render_template("index.html")
@@ -45,53 +42,6 @@ def index():
 @app.route("/jail")
 def jail():
     return render_template("jail.html")
-
-
-# @app.route("/solve4", methods=["GET", "POST"])
-# def solve4():
-#     w1 = None
-#     w2 = None
-
-#     form = WordForm4()
-#     if form.validate_on_submit():
-#         w1 = form.w1.data
-#         w2 = form.w2.data
-#         # return redirect( url_for('actor', id=id) )rm()
-
-#     solution = ""
-#     if w1 is not None and w2 is not None:
-#         print(w1, w2)
-#         solution = analysis.weaver_analysis.gui_paths_4(w1.lower(), w2.lower())
-#         # print(solution)
-
-#     return render_template("solve4.html", form=form, solution=solution)
-
-def generate_solution_html(w1, w2):
-    
-    paths = analysis.weaver_analysis.correctness_paths_4(w1, w2)
-    path_box = ""
-    for p_num, p in enumerate(paths):
-        option_string = ""
-        option_string += f'<div class="path-option" id="option{p_num+1}Content">\n'
-        option_string += f'<h2 class="option-name">Option {p_num+1}</h2>\n'
-        for step in p:
-            option_string += '<div class="wordle-grid">\n'
-            for letter, correctness in step:
-                if correctness == "C":
-                    option_string += f'<div class="box correct-box">{letter}</div>\n'
-                else:
-                    option_string += f'<div class="box normal-box">{letter}</div>\n'
-            option_string += '</div>\n'
-        option_string += '</div>\n'
-        path_box += option_string
-
-
-    selector = ""
-    for p_num in range(len(paths)):
-        selector += f'<option value="option{p_num+1}">Option {p_num+1}</option>\n'
-    selector += f'<option value="all" selected>All</option>\n'
-
-    return selector, path_box
 
 
 @app.route("/solve", methods=["GET", "POST"])
@@ -112,11 +62,6 @@ def solve():
 
     solution = ""
     if w1 is not None and w2 is not None:
-        # print(w1, w2)
-        # selector, path_box = generate_solution_html(w1, w2)
-        # solution_present = True
-        # print(selector)
-        # print(path_box)
         solutions = analysis.weaver_analysis.correctness_paths_4(w1, w2)
         num_solutions = len(solutions)
         print(num_solutions)
